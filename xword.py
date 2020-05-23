309a9adf6c9767465181c3cc64fdad0ca1e68fa5
Howard Post <hpost2012@gmail.com>
2020-05-23 11:55:06 -0700

    implemented word_lookup

--- xword.py
+++ xword.py
@@ -2,20 +2,26 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "hpost2019"
 
-# YOUR HELPER FUNCTION GOES HERE
-
+def word_lookup(test_word, words):
+    word_list = filter(lambda word: len(word) == len(test_word), words)
+    
+    for index in range(len(test_word)):
+        if test_word[index] != " ":
+            word_list = [word for word in word_list if word[index] == test_word[index]]
+    return word_list
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
 
-    test_word = raw_input(
+    test_word = input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
 
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    fin_list = word_lookup(test_word, words)
+    for word in fin_list:
+        print(word)
 
 
 if __name__ == '__main__':
